322d9ed9f8b1e9d0a5ba42f226da0a92c02c9269
sofileon <104622642+sofileon@users.noreply.github.com>
2022-12-12 20:35:53 +0100

    Leave only visualization landmark function in landmark_visualization.py

--- utils/landmark_visualization.py
+++ utils/landmark_visualization.py
@@ -18,24 +18,3 @@ def visualization_landmark(landmark, nii_object):
     image_data = nii_object.get_fdata()
     image_data[landmark[:, 0], landmark[:, 1], landmark[:, 2]] = -2000
     nib.viewers.OrthoSlicer3D(image_data, affine=nii_object.affine).show()
-
-
-if __name__ == "__main__":
-    datadir = Path("data/train")
-    images_files_inhale = [i for i in datadir.rglob("*iBHCT.nii.gz") if "copd" in str(i)]
-    images_files = [
-        str(i).replace("BHCT", "e")
-        if "eBH" in str(i)
-        else str(i).replace("iBHCT.nii.gz", "300_iBH_xyz_r1.txt")
-        for i in images_files
-    ]
-    landmark_files = [
-        str(i).replace("eBHCT.nii.gz", "300_eBH_xyz_r1.txt")
-        if "eBH" in str(i)
-        else str(i).replace("iBHCT.nii.gz", "300_iBH_xyz_r1.txt")
-        for i in images_files
-    ]
-
-    images = [nib.load(i) for i in images_files]
-    landmarks = [np.loadtxt(lfile).astype(np.int16) for lfile in landmark_files]
-    visualization_landmark(landmarks[0], images[0])
