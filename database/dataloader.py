6c0d99bab63c869fe10d20fef4b381854c97d8e9
Lluis <lluisb3@gmail.com>
2022-12-15 18:37:20 +0100

    dataloader.py fixed to skip the two first rows in the inhale landmarks .txt files

--- database/dataloader.py
+++ database/dataloader.py
@@ -30,14 +30,14 @@ def data_loader(data_path):
     if "train" in str(data_path):
         landmark_files = [(Path(str(i[0]).replace("iBHCT.nii.gz", "300_iBH_xyz_r1.txt")),
                            Path(str(i[1]).replace("eBHCT.nii.gz", "300_eBH_xyz_r1.txt"))) for i in images_files]
-        landmarks_inhale = [np.loadtxt(lfile[0]).astype(np.int16) for lfile in landmark_files]
-        landmarks_exhale = [np.loadtxt(lfile[1]).astype(np.int16) for lfile in landmark_files]
+        landmarks_inhale = [np.loadtxt(lfile[0], skiprows=2).astype(np.int16) for lfile in landmark_files]
+        landmarks_exhale = [np.loadtxt(lfile[1], skiprows=2).astype(np.int16) for lfile in landmark_files]
         landmarks = {patient_id[i]: (landmarks_inhale[i], landmarks_exhale[i]) for i in range(len(landmark_files))}
         landmarks = pd.DataFrame.from_dict(landmarks, orient='index')
         landmarks = landmarks.rename(columns={0: "inhale", 1: "exhale"})
     else:
         landmark_files = [Path(str(i).replace("iBHCT.nii.gz", "300_iBH_xyz_r1.txt")) for i in images_files_inhale]
-        landmarks = {patient_id[i]: np.loadtxt(landmark_files[i]).astype(np.int16) for i in range(len(landmark_files))}
+        landmarks = {patient_id[i]: np.loadtxt(landmark_files[i], skiprows=2).astype(np.int16) for i in range(len(landmark_files))}
         landmarks = pd.DataFrame.from_dict(landmarks)
         landmarks = pd.DataFrame.from_dict(landmarks, orient='index')
         landmarks = landmarks.rename(columns={0: "inhale"})
