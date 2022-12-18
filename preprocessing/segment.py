b83296b7343edfd9a41d200abd2041c157192eb0
manasikattel <manasikattel1@gmail.com>
2022-12-18 12:45:26 +0100

    Lung Segmentation k-means and post processing to fill holes and remove small object.s

--- preprocessing/segment.py
+++ preprocessing/segment.py
@@ -1,40 +1,39 @@
 import SimpleITK as sitk
 import matplotlib.pyplot as plt
-from skimage import morphology, measure
+from skimage import morphology
 import cv2
 import numpy as np
 from pathlib import Path
 from tqdm import tqdm
 import click
-
+from datetime import datetime
+from .utils import segment_kmeans, remove_small_3D
 
 thispath = Path.cwd().resolve()
 
 
-def segment_kmeans(image, K=3, attempts=10):
-    image_inv = 255 - image
-
-    # slice_inv = cv2.invert(slice)
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-    vectorized = image_inv.flatten()
-    vectorized = np.float32(vectorized) / 255
-
-    ret, label, center = cv2.kmeans(
-        vectorized, K, None, criteria, attempts, cv2.KMEANS_PP_CENTERS
-    )
-    center = np.uint8(center * 255)
-    res = center[label.flatten()]
-    result_image = res.reshape((image.shape))
-    return result_image
-
-
 def fill_chest_cavity(image, vis_each_slice=False):
+    """
+    Fill the chest cavity to obtain the final gantry mask
+
+    Parameters
+    ----------
+    image : ndarray
+        Input image
+    vis_each_slice : bool, optional
+        Boolean to choose whether to visualize each slice when processing,
+         by default False
+
+    Returns
+    -------
+    ndarray
+        Chest cavity filled image
+    """
     image = image.astype(np.uint8)
     filled_image = np.zeros_like(image)
     for i, slice in enumerate(image):
-        all_objects, hierarchy = cv2.findContours(
-               slice, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
-            )
+        all_objects, hierarchy = cv2.findContours(slice, cv2.RETR_TREE,
+                                                  cv2.CHAIN_APPROX_SIMPLE)
         # # Segmented mask
         # # select largest area (should be the skin lesion)
         mask = np.zeros(slice.shape, dtype="uint8")
@@ -64,6 +63,23 @@ def fill_chest_cavity(image, vis_each_slice=False):
 
 
 def remove_gantry(image, segmented, visualize=True):
+    """
+    Remove the gantry in the orginal CT image.
+
+    Parameters
+    ----------
+    image : ndarray
+        Original Image.
+    segmented : ndarray
+        Mask of the gantry.
+    visualize : bool, optional
+        Flag to visualize after removal., by default True
+
+    Returns
+    -------
+    ndarray
+        Gantry removed image.
+    """
     gantry_mask = segmented * (segmented == np.amin(segmented))
     contours = fill_chest_cavity(gantry_mask, vis_each_slice=False)
 
@@ -87,60 +103,29 @@ def remove_gantry(image, segmented, visualize=True):
     return removed, contours
 
 
-def remove_small(lung_only, vis_each_slice=False):
-    lung_only = lung_only.astype(np.uint8)
-    filled_image = np.zeros_like(lung_only)
-    for i, slice in enumerate(lung_only):
-        nb_blobs, im_with_separated_blobs, stats, _ = cv2.connectedComponentsWithStats(
-            slice
-        )
-        sizes = stats[:, -1]
-        sizes = sizes[1:]
-        nb_blobs -= 1
-        min_size = 150
-        area_filtered = np.zeros_like(slice)
-        # for every component in the image, keep it only if it's above min_size
-        for blob in range(nb_blobs):
-            if sizes[blob] >= min_size:
-                # see description of im_with_separated_blobs above
-                area_filtered[im_with_separated_blobs == blob + 1] = 255
+def get_lung_segmentation(segmented, gantry_mask, visualize=False):
+    """
+    Extract lung masks from the masks received from kmeans segmentation.
+    Removes the small objects, and fills holes.
 
-        # kernel = np.ones((9, 9), np.uint8)
-        # im_result = cv2.morphologyEx(area_filtered, cv2.MORPH_CLOSE, kernel)
-        # im_result = cv2.morphologyEx(im_result, cv2.MORPH_DILATE, kernel)
+    Parameters
+    ----------
+    segmented : ndarray
+        segmentation image
+    gantry_mask : ndarray
+        Mask of gantry
+    visualize : bool, optional
+        Flag to visualize the segmentation mask, by default False
 
-        filled_image[i, :, :] = area_filtered
-        if vis_each_slice:
-            fig, ax = plt.subplots(1, 2, figsize=(10, 5))
-
-            # # Plot the left lung mask
-            ax[0].imshow(slice, cmap="gray")
-            ax[0].set_title("slice")
-
-            # # Plot the right lung mask
-            ax[1].imshow(area_filtered, cmap="gray")
-            ax[1].set_title("mask")
-
-            # # Show the figure
-            plt.show()
-    return filled_image
-
-
-def remove_small_3D(lung_only, vis_each_slice=False):
-    lung_only = lung_only.astype(np.uint8)
-    width = 20
-
-    remove_holes = morphology.remove_small_holes(lung_only, area_threshold=width**3)
-    remove_objects = morphology.remove_small_objects(remove_holes, min_size=width**3)
-
-    return remove_objects
-
-
-def get_lung_segmentation(segmented, gantry_mask, visualize=True):
+    Returns
+    -------
+    ndarray
+        Lung mask.
+    """
     lung = segmented * gantry_mask
     lung_only = lung * (lung == np.amax(lung))
-
     holes_filled = remove_small_3D(lung_only, False)
+
     kernel = morphology.ball(6)
     closed = morphology.closing(holes_filled, kernel)
     dilated = morphology.dilation(closed, kernel)
@@ -156,11 +141,11 @@ def get_lung_segmentation(segmented, gantry_mask, visualize=True):
         ax[1].imshow(holes_filled[60, :, :], cmap="gray")
         ax[1].set_title("gantry_mask")
 
-        ax[2].imshow(dilated[60, :, :], cmap="gray")
+        ax[2].imshow(holes_filled[60, :, :], cmap="gray")
         ax[2].set_title("lung_only")
         plt.show()
 
-    return holes_filled
+    return dilated
 
 
 @click.command()
@@ -174,7 +159,8 @@ def get_lung_segmentation(segmented, gantry_mask, visualize=True):
     "--mask_creation",
     default=False,
     prompt="Gantry mask creation(bool)",
-    help="whether to save the binary mask(True) or the CT image with the gantry removed(False) ; False, True",
+    help=
+    "whether to save the binary mask(True) or the CT image with the gantry removed(False) ; False, True",
 )
 def main(train_type, mask_creation):
     datadir = thispath / Path(f"data/{train_type}")
@@ -187,21 +173,48 @@ def main(train_type, mask_creation):
         img_255 = sitk.Cast(sitk.RescaleIntensity(ct_image), sitk.sitkUInt8)
         seg_img = sitk.GetArrayFromImage(img_255)
         segmented = segment_kmeans(seg_img)
-        gantry_mask = segmented * (segmented == np.amin(segmented))
-        contours = fill_chest_cavity(gantry_mask, vis_each_slice=False)
-        removed = remove_gantry(seg_img, contours, visualize=False)
-        if mask_creation:
-            img_corr = sitk.GetImageFromArray(contours)
-            img_corr.CopyInformation(ct_image)
-            save_dir = thispath / Path(f"data/train_segmentation/{Path(image_file.parent.name)}")
+        removed, gantry_mask = remove_gantry(seg_img,
+                                             segmented,
+                                             visualize=False)
+        lung_mask = get_lung_segmentation(segmented, gantry_mask)
+        save_gantry_removed = True
+        save_lung_mask = True
+
+        if save_lung_mask:
+            lung_mask = sitk.GetImageFromArray(lung_mask.astype(np.uint8))
+            lung_mask.CopyInformation(ct_image)
+            save_dir = thispath / Path(
+                f"data/{train_type}_segmentation_ours/{Path(image_file.parent.name)}"
+            )
             save_dir.mkdir(parents=True, exist_ok=True)
-            sitk.WriteImage(img_corr, str(Path(save_dir / f'seg_body_{image_file.name}')))
+            sitk.WriteImage(
+                lung_mask, str(Path(save_dir / f'seg_lung_{image_file.name}')))
+
+        if save_gantry_removed:
+            removed_sitk = sitk.GetImageFromArray(removed)
+            removed_sitk.CopyInformation(ct_image)
+            save_dir = thispath / Path(
+                f"data/{train_type}_gantry_removed/{Path(image_file.parent.name)}"
+            )
+            save_dir.mkdir(parents=True, exist_ok=True)
+            sitk.WriteImage(removed_sitk,
+                            str(Path(save_dir / f'{image_file.name}')))
+
+        if mask_creation:
+            img_corr = sitk.GetImageFromArray(gantry_mask)
+            img_corr.CopyInformation(ct_image)
+            save_dir = thispath / Path(
+                f"data/train_segmentation/{Path(image_file.parent.name)}")
+            save_dir.mkdir(parents=True, exist_ok=True)
+            sitk.WriteImage(
+                img_corr, str(Path(save_dir / f'seg_body_{image_file.name}')))
         else:
             img_corr = sitk.GetImageFromArray(removed)
             img_corr.CopyInformation(ct_image)
             save_dir = results_dir / Path(image_file.parent.name)
             save_dir.mkdir(parents=True, exist_ok=True)
-            sitk.WriteImage(img_corr, str(Path(save_dir / Path(image_file.name))))
+            sitk.WriteImage(img_corr,
+                            str(Path(save_dir / Path(image_file.name))))
 
 
 if __name__ == "__main__":
