ff564f35db5320e05629180c43f54b25950f7044
manasikattel <manasikattel1@gmail.com>
2022-12-19 18:40:20 +0100

    move plotting to utils/plots; consistent dataset_option

--- preprocessing/segment_unet.py
+++ preprocessing/segment_unet.py
@@ -11,22 +11,21 @@ thispath = Path.cwd().resolve()
 
 @click.command()
 @click.option(
-    "--data_dir",
-    default="train",
-    prompt="Data path",
+    "--dataset_option",
+    default="train_Normalized_CLAHE_gantry_removed",
     help="name of the data folder; train, test, train_Normalized_CLAHE etc",
 )
-def segment_unet(data_dir="train_gantry_removed"):
+def segment_unet(dataset_option):
     """
     Segment the lung CT images using Unet based segmentation model
     Parameters
     ----------
-    data_dir : str, optional
+    dataset_option : str, optional
         Name of the directory to look for the images in "data", by default "train_gantry_removed"
     """
-    datadir = thispath / Path(f"data/{data_dir}")
+    datadir = thispath / Path(f"data/{dataset_option}")
     images_files = [i for i in datadir.rglob("*.nii.gz") if "copd" in str(i)]
-    dataset_ = data_dir.split('_')[0]
+    dataset_ = dataset_option.split('_')[0]
     results_dir = thispath / Path(f"data/{dataset_}_segmentations")
     results_dir.mkdir(parents=True, exist_ok=True)
     # Read the chest CT scan
@@ -53,9 +52,8 @@ def segment_unet(data_dir="train_gantry_removed"):
         sitk.WriteImage(
             img_corr,
             str(
-                Path(
-                    save_dir /
-                    Path(f"seg_lung_unet_{image_file.stem.split('.')[0]}.nii.gz"))))
+                Path(save_dir / Path(
+                    f"seg_lung_unet_{image_file.stem.split('.')[0]}.nii.gz"))))
 
 
 if __name__ == "__main__":
--- utils/__init__.py
+++ utils/__init__.py
@@ -3,9 +3,11 @@ from .metrics import TRE_measure, metrics_4_all
 from .landmark_visualization import visualization_landmark
 from .batchfile_creator import elastix_batch_file, transformix_batch_file
 from .preprocess_utils import segment_kmeans, remove_small_objects, remove_small_3D
+from .plots import hist_plotting
 
 __all__ = [
     "get_image_info", "read_raw", "TRE_measure", "metrics_4_all",
     "visualization_landmark", "elastix_batch_file", "transformix_batch_file",
-    "segment_kmeans", "remove_small_objects", "remove_small_3D"
+    "segment_kmeans", "remove_small_objects", "remove_small_3D",
+    "hist_plotting"
 ]
